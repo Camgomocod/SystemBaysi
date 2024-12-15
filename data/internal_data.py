707ac1222ca3dd0cbfc4fa70502b372ce2c3cb9d
Chancar <camgomocod@gmail.com>
2024-12-15 18:41:53 -0500

    Estructura del proyecto

---
+++ data/internal_data.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class InternalData:
+    def __init__(self) -> None:
+        pass
+
+    def set_ip(self, key: str, value: str) -> None:
+        os.environ[key] = value
+        try:
+            with open("/asssets/.env", "r") as file:
+                lines = file.readlines()
+
+            with open("assets/.env", "w") as file:
+                for line in lines:
+                    if line.startswith(f"{key}="):
+                        file.write(f"{key}={value}\n")
+                    else:
+                        file.write(line)
+        except Exception as e:
+            print(f"Exception {e}")
+
+    def get_ip(self) -> str:
+        return os.getenv("IP_ADDRESS", "0.0.0.0")
+
+    def get_port(self) -> int:
+        return int(os.getenv("PORT", "9999"))
